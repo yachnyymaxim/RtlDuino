5a3015d4a551159eea5009b6c2b9c590d08bf6aa
Alexander Voronin <av1024@gmail.com>
2018-01-15 17:41:22 +0300

    Fix missed JLink-OTA script

--- development/rtl87xx/tools/JLinkGDB_OTA.sh
+++ development/rtl87xx/tools/JLinkGDB_OTA.sh
@@ -3,24 +3,20 @@
 if [ ! -d "${1}" ]; then
  # goto no_dir
   JLinkGDBServer -device Cortex-M3 -if SWD -ir -endian little -speed 3500 &
-  arm-none-eabi-gdb -x gdb_img.jlink
+  arm-none-eabi-gdb -x gdb_ota.jlink
   killall JLinkGDBServer
   exit 0
 fi
 
 if [ ! -f "${1}/ota.bin" ]; then
-  # if not exist %1\ota.bin goto error_x
   echo "Error: Not found ${1}/ota.bin!"
   exit 1
 fi
 
-#if exist %2\ota.bin del %2\ota.bin
-#copy /b %1\ota.bin %2\ota.bin
 cp -f "$1/ota.bin" "$2/ota.bin"
 cd "$2"
 JLinkGDBServer -device Cortex-M3 -if SWD -ir -endian little -speed 3500 &
-#%3arm-none-eabi-gdb.exe -x gdb_img.jlink
-${3}arm-none-eabi-gdb -x gdb_img.jlink
+${3}arm-none-eabi-gdb -x gdb_ota.jlink
 killall JLinkGDBServer
 
 
